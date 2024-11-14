f5d8c615bdd2209a73936d752131f32bd0847b56
Michael Travers <miketravers94@gmail.com>
2024-11-13 20:32:06 -0500

    working on display vehical category

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Navbar from './components/navbar/navbar'
 import data from './data/vehicles.json'
@@ -10,8 +9,7 @@ import data from './data/vehicles.json'
 //   year:Number
 // }
 function App() {
-  const [count, setCount] = useState(0)
- const [select, setSelect]=useState()
+
 
   // const handleSubmit=(e)=>{
   //   e.preventDefault()
@@ -74,18 +72,7 @@ function App() {
         
         
       </div>
-      <h1>Vite + React</h1>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     
     </>
   )
 }
