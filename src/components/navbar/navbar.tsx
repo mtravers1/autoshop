de9cd85754aeac1ba8971978f26e7c9b583a23bc
Michael Travers <miketravers94@gmail.com>
2024-11-14 18:50:48 -0500

    added in car selector

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,38 +1,41 @@
+import { useState } from "react"
 import Headnav from "./headnav"
 
 const Navbar = ()=>{
+    const [toggleNav, setToggleNav] = useState(false)
     return(
         <div>
             <Headnav/>
-            <div id="nav" className="">
-                <div id='navtop' className="flex justify-around items-center">
+            <div id="nav" className="sm:hidden">
+                {/* <div id='navtop' className="flex justify-around items-center">
                     <div  id='topleft' className="flex m-3 items-center">
                         <p className="font-bold text-xl">THECARKEYREPLACEMENTCENTER</p>
-                        <span>
+                       
+                    </div>
+                    <div>
                             <input className="border rounded w-80 h-10"/>
                             <button className="bg-red-400 rounded h-10 w-24">search</button>
-                        </span>
-                    </div>
+                        </div>
                     <div id='topright' className="m-9 flex items-center">
-                        <span className="m-5">
+                        <div className="m-5">
                             <p className="">
                                 Hello
                             </p>
                             <p>Sign in</p>
                             
                            
-                        </span>
-                        <span className="m-4">
+                        </div>
+                        <div className="m-4">
                             <p>Wish Lists</p>
                             <p></p>
                             
-                        </span>
-                        <span className="m-5">
+                        </div>
+                        <div className="m-5">
                             Cart
-                        </span>
+                        </div>
 
                     </div>
-                </div>
+                </div> */}
                 
             </div>
         </div>
